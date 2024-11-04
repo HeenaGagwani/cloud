1fec6b67a5f5529a6c29ef9b7c4d45dc98ae547d
Heena Gagwani <49018377+HeenaGagwani@users.noreply.github.com>
2024-11-04 11:59:48 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/predict', methods=['GET', 'POST'])
-def predict():
-    if request.method == 'POST':
-        # Here, you would typically process the input and make predictions
-        # For demonstration, we will just return a dummy prediction
-        input_data = request.form['input_data']  # Assuming you have a form input named 'input_data'
-        prediction = f"Dummy prediction for: {input_data}"
-        return render_template('prediction.html', prediction=prediction)
-    return render_template('prediction.html')
+def hello_world():
+    return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
